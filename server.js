83a7c880929b2844e8be7658c5b7d2f1f9368267
Spencer Dyvig <sd31117@hotmail.com>
2021-07-12 16:53:39 -0500

    set up basic nodejs websocket server with console join message and broadcast function

---
+++ server.js
@@ -0,0 +1,16 @@
+const webS = require("ws")
+const wss = new webS.Server({port:7777})
+
+wss.on("connection", ws=>{
+    console.log(JSON.stringify(wss.clients))
+    console.log("connected client, ip: " + ws._socket.remoteAddress.replace("::ffff:",""))
+    ws.on("message",msg=>{
+        wss.broadcast(JSON.stringify({func:msg}))
+    })
+});
+
+wss.broadcast = function broadcast(msg) {
+    wss.clients.forEach(function each(client) {
+        client.send(msg)
+    });
+};
